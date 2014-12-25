9d2e269ef226fa7a31462e69800377d76e6015c6
曾楚杰 <zzccjj1234@gmail.com>
2014-12-25 17:14:09 +0800

    convert tabs to 2 spaces

--- test.cpp
+++ test.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-	DES Encryptor;
-	DES_Value data = (((DES_Value)0x01234567) << 32) | 0x89abcdef;
-	DES_Value key = (((DES_Value)0x13345779) << 32) | 0x9BBCDFF1;
-	DES_Value inf = Encryptor.Encrypt(key, data, DES_Mode::Encrypt);
-	cout << hex << inf << endl;
-	cout << hex << Encryptor.Encrypt(key,inf,DES_Mode::Decrypt) << endl;
+  DES Encryptor;
+  DES_Value data = (((DES_Value)0x01234567) << 32) | 0x89abcdef;
+  DES_Value key = (((DES_Value)0x13345779) << 32) | 0x9BBCDFF1;
+  DES_Value inf = Encryptor.Encrypt(key, data, DES_Mode::Encrypt);
+  cout << hex << inf << endl;
+  cout << hex << Encryptor.Encrypt(key,inf,DES_Mode::Decrypt) << endl;
 }
