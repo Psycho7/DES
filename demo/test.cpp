c4c1f0baf97989c088cb214c5e42467978c0e937
Chujie Zeng <chujie.zeng@gmail.com>
2015-03-24 01:35:20 +0800

    Unix Style

--- demo/test.cpp
+++ demo/test.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "DES.h"
+#include "../src/DES.h"
 
 using namespace std;
 
 int main()
 {
   DES Encryptor;
-  DES_Value data = (((DES_Value)0x01234567) << 32) | 0x89abcdef;
+  DES_Value data = (((DES_Value)0x01234567) << 32) | 0x89abcdef;  // 0x0123456789abcdef
   DES_Value key = (((DES_Value)0x13345779) << 32) | 0x9BBCDFF1;
   DES_Value inf = Encryptor.Encrypt(key, data, DES_Mode::Encrypt);
   cout << hex << inf << endl;
